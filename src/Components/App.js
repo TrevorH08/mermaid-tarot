3e4ef3f73b316b3286314b07d01856ab20d8f489
TrevorH08 <trevorh8700@gmail.com>
2023-01-18 12:13:11 -0800

    add basic fragment for app and header, still restudying

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Header />
+      <h1>Welcome to Mermaid Tarot</h1>
+      <h2>Take a look at the different packages and book below!</h2>
+      <hr/>
+    </React.Fragment>
   );
 }
 
