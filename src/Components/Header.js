00175c636f07b72ffecaf2731e3e4d24d21b6ca5
TrevorH08 <trevorh8700@gmail.com>
2023-01-19 12:17:09 -0800

    WIP tweaking header looking, trying to incorporate logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from './../img/mermaid-tarot-logo.png';
 
 function Header(){
   const headerStyle = {
@@ -16,6 +17,7 @@ function Header(){
   return (
     <React.Fragment>
       <Navbar className="navbar fixed-top navbar-light bg-light py-0" style={headerStyle}>
+        <img src="Logo"></img>
         <h1>Mermaid Tarot</h1>
       </Navbar>
     </React.Fragment>
